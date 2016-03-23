a63fc03fae2af5718a527ecd28ca1563cc2ada00
Cheyenne Kellis <cheyenne.kellis@gmail.com>
2016-03-23 15:16:21 -0700

    Fix downloadUrls

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -60,5 +60,8 @@ exports.isUrlArchived = function(url, callback) {
 };
 
 exports.downloadUrls = function(urls) {
-
+  _.each(urls, function(url) {
+    if (!url) return;
+    request('http://' + url).pipe(fs.createWriteStream(exports.paths.archivedSites + "/" + url));
+  });
 };
